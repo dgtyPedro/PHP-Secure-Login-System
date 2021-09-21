2105a3ab796a4186c0b86dc000987e1e04a7264d
dgtyPedro <pedrogcs0104@gmail.com>
2021-09-21 10:47:24 -0300

    feat: login estruturado

--- php/login.php
+++ php/login.php
@@ -2,3 +2,43 @@
 session_start();
 require_once 'config.php';
 
+
+if ($stmt = $conn->prepare('SELECT id, password FROM users WHERE email = ?')) {
+	$stmt->bind_param('s', $_POST['email']);
+	$stmt->execute();
+	$stmt->store_result();
+
+	if ($stmt->num_rows > 0) {
+		$stmt->bind_result($id, $password);
+		$stmt->fetch();
+
+		if (password_verify($_POST['password'], $password)) {
+			session_regenerate_id();
+			$_SESSION['loggedin'] = TRUE;
+			$_SESSION['name'] = $_POST['email'];
+			$_SESSION['id'] = $id;
+			?>
+			<script>
+			javascript:alert('Logado!');
+			</script>
+			<?php
+
+			
+		} else {
+			// Incorrect password
+			?>
+			<script>
+			javascript:alert('Senha errada!');
+			</script>
+			<?php
+		}
+	} else {
+		// Incorrect email
+		?>
+		<script>
+		javascript:alert('Email errado!');
+		</script>
+		<?php
+	}
+	$stmt->close();
+}	
